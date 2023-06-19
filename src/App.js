05a989c1843d2b123fb3cf88aa33091c840c50e0
Hanne <hanne273@gmail.com>
2023-06-19 20:05:54 +0200

    Added css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Heading</header>
+        <header className="App-header">Dictionary</header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="lime" />
         </main>
       </div>
 
